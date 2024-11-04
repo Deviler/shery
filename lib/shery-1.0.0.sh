695e5dea9cd4b8bba4ab5850fdd3908518e4ac4a
Deviler <1779557358@qq.com>
2024-11-05 00:37:49 +0800

    创建shery-1.0.0.sh

---
+++ lib/shery-1.0.0.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+## shery-1.0.0.sh  脚本库开发    注释必须使用##符号  脚本中字符串不要出现##字符
+## shery-1.0.1.sh  统一函数命名  S_动_宾  模式命名
+## shery-1.0.2.sh  消除eval命令  统一使用高版本bash支持的declare -n实现
+## 主要思想：将所有数据转换为list列表进行处理，如同lua中的table进行各种逻辑实现
